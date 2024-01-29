f3b27ad395a1254f6ea6392ee6e6b7bb3fe3ec7f
JadeBoudarel <jade.boudarel@gmail.com>
2024-01-29 11:45:46 +0100

    add js in html

--- app/static/main.js
+++ app/static/main.js
@@ -1,23 +1,2 @@
-$( document ).ready(function() {
- 
-    $( "#btn1" ).click(function( event ) {
-   
-        alert( "im win" );
-   
-        event.preventDefault();
-    });
-   });
-
-
-
-   fetch('/velo_data')
-  .then(response => response.json())
-  .then(data => {
-    const formattedData = JSON.stringify(data, null, 2);
-
-
-        document.getElementById('content').innerHTML = `<pre>${formattedData}</pre>`;
-
-      })
 
 
